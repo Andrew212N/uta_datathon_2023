d45db9dc07bc8be3b85a5959ba16ebdd56faaa55
Andrew212N <112042176+Andrew212N@users.noreply.github.com>
2023-04-16 08:25:42 +0000

    search function works :3

--- B2/main.py
+++ B2/main.py
@@ -1,20 +1,25 @@
 import pandas as pd
-from textblob import TextBlob
 
+print('Find Health Tweets containing a specific medical term.\nInitializing word bank and Tweet bank...')
 words_dictionary = {}
 with open('wordlist.txt', 'r') as file:
     for line in file:
         words_dictionary[line.strip()] = 1
 
-xlsx = pd.read_excel("TwitterHealthData.xlsx", dtype={'tweet_id':'int', 'tweet_text':'str'})
-
-def get_sentiment(text):
-    blob = TextBlob(text)
-    return blob.sentiment.polarity
-
-    xlsx['Sentiment'] = xlsx['Text'].apply(get_sentiment)
+xlsx = pd.read_excel("TwitterHealthData.xlsx", usecols=['tweet_text'])
+dataset = list(xlsx.iloc[:,0])
 
 search = input('Enter a search term: ')
+print('Printing out all the tweets that contain the word: ' + search +  '\nPlease wait a few seconds...\n')
+search = search.lower()
 
 if search in words_dictionary:
-        matches = []
+    matches = [entry for entry in dataset if any(word.lower() in entry.lower() for word in words_dictionary) and search in entry.lower()]
+    if matches:
+        for entry in matches:
+            print(entry + '\n')
+        print(f'Found {len(matches)} tweets')
+    else:
+        print('No entries found')
+else:
+    print(f'{search} is not in the word bank\nTry using different capitalization or dashes where there should be.')
