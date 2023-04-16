458004660ed3e77efebb6a3fe17653f903bd32b0
Andrew212N <nguyen.andrew1234@yahoo.com>
2023-04-15 21:45:52 -0500

    idk

--- B2/main.py
+++ B2/main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from textblob import TextBlob
-import random
+
+words_dictionary = {}
+with open('wordlist.txt', 'r') as file:
+    for line in file:
+        words_dictionary[line.strip()] = 1
 
 xlsx = pd.read_excel("TwitterHealthData.xlsx", dtype={'tweet_id':'int', 'tweet_text':'str'})
 
@@ -10,9 +14,7 @@ def get_sentiment(text):
 
     xlsx['Sentiment'] = xlsx['Text'].apply(get_sentiment)
 
-num_rows = len(xlsx)
+search = input('Enter a search term: ')
 
-for i in range(20):
-    rand_num = random.randint(0, num_rows-1)
-    print('Tweet ID: ' + str(xlsx['tweet_id'].iloc[rand_num]))
-    print(xlsx['tweet_text'].iloc[rand_num] + '\n')
+if search in words_dictionary:
+        matches = []
