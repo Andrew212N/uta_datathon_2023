c375e3c0cf3b07ee40e25e8d08ad5bd60cf52b88
Andrew212N <nguyen.andrew1234@yahoo.com>
2023-04-15 19:43:40 -0500

    printing random tweets, specification To do

--- B2/main.py
+++ B2/main.py
@@ -1,4 +1,18 @@
 import pandas as pd
+from textblob import TextBlob
+import random
 
-xlsx_file = pd.ExcelFile("TwitterHealthData.xlsx")
-print(xlsx_file)
+xlsx = pd.read_excel("TwitterHealthData.xlsx", dtype={'tweet_id':'int', 'tweet_text':'str'})
+
+def get_sentiment(text):
+    blob = TextBlob(text)
+    return blob.sentiment.polarity
+
+    xlsx['Sentiment'] = xlsx['Text'].apply(get_sentiment)
+
+num_rows = len(xlsx)
+
+for i in range(20):
+    rand_num = random.randint(0, num_rows-1)
+    print('Tweet ID: ' + str(xlsx['tweet_id'].iloc[rand_num]))
+    print(xlsx['tweet_text'].iloc[rand_num] + '\n')
